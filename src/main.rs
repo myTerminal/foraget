69789179ad180a515d8842ec5dd788b24c399da7
myTerminal <team.terminal@gmail.com>
2020-05-12 21:17:43 -0500

    use 'clap' to define basic usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .subcommand(
+            SubCommand::with_name("init")
+                .about("Install additional package sources"),
+        )
+        .subcommand(
+            SubCommand::with_name("search")
+                .about("Search for a package across sources")
+                .arg(
+                    Arg::with_name("PACKAGE")
+                        .help("The package to search")
+                        .required(true)
+                        .index(1),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("install")
+                .about("Install a package")
+                .arg(
+                    Arg::with_name("PACKAGE")
+                        .help("The package to install")
+                        .required(true)
+                        .index(1),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("uninstall")
+                .about("Uninstall a package if installed")
+                .arg(
+                    Arg::with_name("PACKAGE")
+                        .help("The package to uninstall")
+                        .required(true)
+                        .index(1),
+                ),
+        )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("init") {
+        println!("Initializing more package sources...");
+    } else if let Some(matches) = matches.subcommand_matches("search") {
+        let package_to_search = matches.value_of("PACKAGE").unwrap();
+
+        println!("Searching {}...", package_to_search);
+    } else if let Some(matches) = matches.subcommand_matches("install") {
+        let package_to_install = matches.value_of("PACKAGE").unwrap();
+
+        println!("Installing {}...", package_to_install);
+    } else if let Some(matches) = matches.subcommand_matches("uninstall") {
+        let package_to_uninstall = matches.value_of("PACKAGE").unwrap();
+
+        println!("Uninstalling {}...", package_to_uninstall);
+    } else {
+        println!("Please run {} with a command.", crate_name!());
+    }
 }
